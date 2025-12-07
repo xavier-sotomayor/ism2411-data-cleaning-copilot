259b383f58b23bf18ba3717e9a132f81847bcc0a
xavier-sotomayor <xaviersotomayor@usf.edu>
2025-12-07 13:37:11 -0500

    Add files via upload
    Finished the cleaning part of the script. Included 4 functions that removed white space from the data, made all of the column names uppercase, filled in missing values with 'N/A', and removed rows with invalid values.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,12 +1,39 @@
 import pandas as pd
 import numpy as np
 import csv
+print(np.__version__)
 
-with open('sales_data_raw.csv', 'r') as file:
-    reader = csv.reader(file)
-    headers = next(reader)
-    data = [row for row in reader]
-print(data)
+df = pd.read_csv('sales_data_raw.csv')
 
 #Standardize column names
-headers = [header.upper().replace(' ', '_') for header in headers]
+def clean_column_names(df):
+    df.columns = df.columns.str.strip().str.upper().str.replace(' ', '_')
+    return df
+
+df = clean_column_names(df)
+
+#Clean the white spaces in the data
+def clean_white_space(df):
+    df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
+    return df
+
+df = clean_white_space(df)
+
+#Deal with missing values
+def handle_missing_values(df):
+    df = df.fillna('N/A')
+    return df
+
+df = handle_missing_values(df)
+
+df['QTY'] = pd.to_numeric(df['QTY'], errors='coerce')
+df['PRICE'] = pd.to_numeric(df['PRICE'], errors='coerce')
+
+#Deal with rows with negative quantity or price
+def remove_invalid_rows(df):
+    df = df.dropna(subset=['QTY', 'PRICE'])
+    df = df[(df['QTY'] >= 0) & (df['PRICE'] >= 0)]        
+    return df
+
+df = remove_invalid_rows(df)
+
